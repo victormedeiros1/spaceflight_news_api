b8ac5d5efbb8631329dfe411de23c0b964cbc134
victormedeiros1 <josevictordev@gmail.com>
2023-07-11 20:44:02 -0300

    feat: listagem das notícias

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
+  runApp(MyApp());
+}
 
-  MaterialApp app = MaterialApp(
+class MyApp extends StatelessWidget {
+  Future<List<dynamic>> fetchSpaceflightNews() async {
+    final response = await http
+        .get(Uri.parse('https://api.spaceflightnewsapi.net/v4/articles'));
 
+    if (response.statusCode == 200) {
+      // Decodifica a resposta JSON
+      final List<dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Falha ao carregar os dados da API');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData(
-
         primarySwatch: Colors.purple,
-
       ),
-
-      home: Text("Apenas começando..."));
-
-  runApp(app);
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Spaceflight News'),
+        ),
+        body: FutureBuilder<List<dynamic>>(
+          future: fetchSpaceflightNews(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // Exibe a lista de notícias
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final news = snapshot.data![index];
+                  return ListTile(
+                    title: Text(news['title']),
+                    subtitle: Text(news['summary']),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              // Exibe uma mensagem de erro caso haja falha na busca dos dados
+              return Center(
+                child: Text('Erro ao carregar os dados da API'),
+              );
+            }
+            // Exibe um indicador de progresso enquanto os dados são buscados
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
