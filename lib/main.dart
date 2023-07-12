48a49bbfa1684e8549e75c4f490857e9e7820396
victormedeiros1 <josevictordev@gmail.com>
2023-07-11 21:01:59 -0300

    feat: adicionando scroll infinito e puxando mais notícias ao rolar a página
    Co-authored-by: guilhermecostam <guilhermecostadev@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,44 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ScrollController _scrollController = ScrollController();
+  List<dynamic> _newsList = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSpaceflightNews();
+    _scrollController.addListener(_scrollListener);
+  }
+
   Future<List<dynamic>> fetchSpaceflightNews() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     final response = await http
         .get(Uri.parse('https://api.spaceflightnewsapi.net/v3/articles'));
 
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
-      return data;
+      setState(() {
+        _newsList.addAll(data);
+        _isLoading = false;
+      });
     } else {
+      setState(() {
+        _isLoading = false;
+      });
       throw Exception('Falha ao carregar os dados da API');
     }
+    return _newsList;
   }
 
   void _navigateToNewsDetails(BuildContext context, Map<String, dynamic> news) {
@@ -29,6 +56,21 @@ class MyApp extends StatelessWidget {
     );
   }
 
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      fetchSpaceflightNews();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,45 +79,39 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Spaceflight News - Nóticias sobre o espaço!'),
+          title: Text('Spaceflight News'),
         ),
-        body: FutureBuilder<List<dynamic>>(
-          future: fetchSpaceflightNews(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final news = snapshot.data![index];
-                  final description =
-                      (news['summary'] as String).split(' ').take(20).join(' ');
-                  final isDescriptionTruncated =
-                      (news['summary'] as String).split(' ').length > 20;
-                  final truncatedDescription =
-                      isDescriptionTruncated ? '$description...' : description;
+        body: ListView.builder(
+          controller: _scrollController,
+          itemCount: _newsList.length + 1,
+          itemBuilder: (context, index) {
+            if (index < _newsList.length) {
+              final news = _newsList[index];
+              final description =
+                  (news['summary'] as String).split(' ').take(20).join(' ');
+              final isDescriptionTruncated =
+                  (news['summary'] as String).split(' ').length > 20;
+              final truncatedDescription =
+                  isDescriptionTruncated ? '$description...' : description;
 
-                  return ListTile(
-                    title: Text(news['title']),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(truncatedDescription),
-                        if (isDescriptionTruncated)
-                          Text('Clique para ver mais'),
-                      ],
-                    ),
-                    onTap: () => _navigateToNewsDetails(context, news),
-                  );
-                },
+              return ListTile(
+                title: Text(news['title']),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(truncatedDescription),
+                    if (isDescriptionTruncated) Text('Clique para ver mais'),
+                  ],
+                ),
+                onTap: () => _navigateToNewsDetails(context, news),
               );
-            } else if (snapshot.hasError) {
+            } else if (_isLoading) {
               return Center(
-                child: Text('Erro ao carregar os dados da API'),
+                child: CircularProgressIndicator(),
               );
+            } else {
+              return Container();
             }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
           },
         ),
       ),
